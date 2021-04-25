445e6a89b02a88df8d50a33a4c83f6f6d1fe3182
chlavieri <chlavieri@github.com>
2021-04-24 22:54:25 -0300

    Solved #12 (cheating?)

--- project_euler_12.py
+++ project_euler_12.py
@@ -5,52 +5,24 @@ Created on Sat May 16 21:03:25 2020
 @author: Carlos Henrique
 """
 
-list_triangle_numbers = []
+from sympy import divisors
+i = 0
+triangle = 0
+div = set()
 
-# def triangle_numbers(n):
-#     for i in range(1,n):
-#         j = (i*(i+1))/2
-#         j = int(j)
-#         list_triangle_numbers.append(j)
+while len(div) <= 500:
+    div = set()
+    i += 1
+    triangle = triangle + i
+    div = list(divisors(triangle))
+    # for j in range(1, int(math.sqrt(triangle)+ 1)):
+    #     if (triangle % j == 0):
+    #         div.update([i, triangle // j])
+    if (len(div) >= 500):
+        print(triangle)
    
-#     for j in list_triangle_numbers:
-#         divisors = []
-#         for k in range(1, j):
-#             if j%k == 0:
-#                 divisors.append(k)
-#                 if len(divisors) == 5:
-#                     print("The value of the number with over 5 divisors is", j)
-#                     break
-    
-    
-# triangle_numbers(10)
-        
+   
 
-import math
-
-for n in range(100000):
-    divisors = []
-    triangle = (n*(n+1))/2
-    triangle = int(triangle)
-    i = 1
-    while (i <= math.sqrt(triangle)):
-        if n%i == 0:
-            if (n/i == i):
-                divisors.append(i)
-            else:
-                divisors.append(i)
-                divisors.append(n/i)
-            if len(divisors) > 500:
-                print(triangle)
-                break
-        i += 1
-           
-   
-   
-   
-   
-   
-   
    
    
    
